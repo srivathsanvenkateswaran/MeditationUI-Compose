bd79407a1f94a94c9bcabc0bbd7a67a3ff5c2b9e
srivathsanvenkateswaran <srivathsanvenkateswaran@gmail.com>
2021-07-21 20:29:06 +0530

    Called HomeScreen() in MainActivity.kt

--- app/src/main/java/com/srivathsanvenkateswaran/meditationui/MainActivity.kt
+++ app/src/main/java/com/srivathsanvenkateswaran/meditationui/MainActivity.kt
@@ -3,19 +3,22 @@ package com.srivathsanvenkateswaran.meditationui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.srivathsanvenkateswaran.meditationui.ui.composables.HomeScreen
 import com.srivathsanvenkateswaran.meditationui.ui.theme.MeditationUITheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MeditationUITheme {
-
+                HomeScreen()
             }
         }
     }
