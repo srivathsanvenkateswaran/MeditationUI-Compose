0de2c2dd3029efb6268698bf1da2cb9080c3bb79
srivathsanvenkateswaran <srivathsanvenkateswaran@gmail.com>
2021-07-21 19:52:51 +0530

    Added FeaturesSection()

--- app/src/main/java/com/srivathsanvenkateswaran/meditationui/ui/composables/HomeScreen.kt
+++ app/src/main/java/com/srivathsanvenkateswaran/meditationui/ui/composables/HomeScreen.kt
@@ -1,30 +1,41 @@
 package com.srivathsanvenkateswaran.meditationui.ui.composables
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.srivathsanvenkateswaran.meditationui.R
 import com.srivathsanvenkateswaran.meditationui.ui.theme.*
-import com.srivathsanvenkateswaran.meditationui.utils.DummyData
+import androidx.compose.foundation.Canvas
+import androidx.compose.material.*
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.srivathsanvenkateswaran.meditationui.utils.DummyData.chips
+import com.srivathsanvenkateswaran.meditationui.utils.DummyData.features
 import com.srivathsanvenkateswaran.meditationui.utils.DummyData.name
+import com.srivathsanvenkateswaran.meditationui.utils.Feature
+import com.srivathsanvenkateswaran.meditationui.utils.standardQuadFromTo
 
+@ExperimentalFoundationApi
 @Composable
 fun HomeScreen() {
     Surface(
@@ -40,6 +51,142 @@ fun HomeScreen() {
             ChipSection()
 
             CurrentMeditation()
+
+            FeaturesSection(features)
+        }
+    }
+}
+
+@ExperimentalFoundationApi
+@Composable
+private fun FeaturesSection(
+    features: List<Feature>
+) {
+    Text(
+        text = "Features",
+        style = MaterialTheme.typography.h1,
+        modifier = Modifier
+            .padding(15.dp)
+    )
+
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(2),
+        contentPadding = PaddingValues(start = 7.5.dp, end = 7.5.dp, bottom = 100.dp),
+        modifier = Modifier
+            .fillMaxHeight()
+    ) {
+        items(features) { feature ->
+            FeatureItem(feature)
+        }
+    }
+}
+
+@Composable
+private fun FeatureItem(
+    feature: Feature
+) {
+    BoxWithConstraints(
+        modifier = Modifier
+            .padding(7.5.dp)
+            .aspectRatio(1f)
+            .clip(RoundedCornerShape(10.dp))
+            .background(feature.darkColor)
+    ) {
+        val width = constraints.maxWidth
+        val height = constraints.maxHeight
+
+        // Medium colored path
+        val mediumColoredPoint1 = Offset(0f, height * 0.3f)
+        val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
+        val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
+        val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
+        val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
+
+        val mediumColoredPath = Path().apply {
+            moveTo(
+                x = mediumColoredPoint1.x,
+                y = mediumColoredPoint1.y
+            )
+            standardQuadFromTo(mediumColoredPoint1, mediumColoredPoint2)
+            standardQuadFromTo(mediumColoredPoint2, mediumColoredPoint3)
+            standardQuadFromTo(mediumColoredPoint3, mediumColoredPoint4)
+            standardQuadFromTo(mediumColoredPoint4, mediumColoredPoint5)
+            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(-100f, height.toFloat() + 100f)
+            close()
+        }
+
+        // Light colored path
+        val lightPoint1 = Offset(0f, height * 0.35f)
+        val lightPoint2 = Offset(width * 0.1f, height * 0.4f)
+        val lightPoint3 = Offset(width * 0.3f, height * 0.35f)
+        val lightPoint4 = Offset(width * 0.65f, height.toFloat())
+        val lightPoint5 = Offset(width * 1.4f, -height.toFloat() / 3f)
+
+        val lightColoredPath = Path().apply {
+            moveTo(lightPoint1.x, lightPoint1.y)
+            standardQuadFromTo(lightPoint1, lightPoint2)
+            standardQuadFromTo(lightPoint2, lightPoint3)
+            standardQuadFromTo(lightPoint3, lightPoint4)
+            standardQuadFromTo(lightPoint4, lightPoint5)
+            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
+            lineTo(-100f, height.toFloat() + 100f)
+            close()
+        }
+
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            drawPath(
+                path = mediumColoredPath,
+                color = feature.mediumColor
+            )
+            drawPath(
+                path = lightColoredPath,
+                color = feature.lightColor
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(15.dp)
+        ) {
+            Text(
+                text = feature.title,
+                style = MaterialTheme.typography.h2,
+                fontSize = 22.sp,
+                modifier = Modifier
+                    .align(Alignment.TopStart),
+                letterSpacing = 1.sp
+            )
+
+            Icon(
+                painter = painterResource(id = feature.iconId),
+                contentDescription = feature.title,
+                tint = Color.White,
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+            )
+
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(ButtonBlue),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = ButtonBlue
+                )
+            ) {
+                Text(
+                    text = "Start",
+                    color = TextWhite,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
         }
     }
 }
@@ -81,8 +228,8 @@ private fun GreetingSection() {
         headingText = "Good Morning, ${name}",
         subHeadingText = "We wish you have a good day!",
         rowModifier = Modifier
-            .fillMaxWidth().
-            padding(15.dp),
+            .fillMaxWidth()
+            .padding(15.dp),
     )
 }
 
@@ -196,6 +343,7 @@ private fun TextSectionColumn(
     }
 }
 
+@ExperimentalFoundationApi
 @Preview
 @Composable
 fun HomeScreenPreview() {
--- app/src/main/java/com/srivathsanvenkateswaran/meditationui/utils/DummyData.kt
+++ app/src/main/java/com/srivathsanvenkateswaran/meditationui/utils/DummyData.kt
@@ -1,5 +1,8 @@
 package com.srivathsanvenkateswaran.meditationui.utils
 
+import com.srivathsanvenkateswaran.meditationui.R
+import com.srivathsanvenkateswaran.meditationui.ui.theme.*
+
 object DummyData {
     var chips: List<String> = mutableListOf(
         "Sweet sleep",
@@ -8,4 +11,35 @@ object DummyData {
     )
 
     var name: String = "Srivathsan"
+
+    var features: List<Feature> = listOf(
+        Feature(
+            title = "Sleep meditation",
+            R.drawable.ic_headphone,
+            BlueViolet1,
+            BlueViolet2,
+            BlueViolet3
+        ),
+        Feature(
+            title = "Tips for sleeping",
+            R.drawable.ic_videocam,
+            LightGreen1,
+            LightGreen2,
+            LightGreen3
+        ),
+        Feature(
+            title = "Night island",
+            R.drawable.ic_headphone,
+            OrangeYellow1,
+            OrangeYellow2,
+            OrangeYellow3
+        ),
+        Feature(
+            title = "Calming sounds",
+            R.drawable.ic_headphone,
+            Beige1,
+            Beige2,
+            Beige3
+        )
+    )
 }
